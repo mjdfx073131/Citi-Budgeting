f2e01b8fa062f99d3e08692fec4eb0e52a7be8fe
sun-yuhan <yuhan25@hotmail.com>
2021-06-16 15:44:59 -0400

    update delete for budget app  db

--- todos/delete.js
+++ todos/delete.js
@@ -6,13 +6,13 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.delete = (event, context, callback) => {
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
+    TableName: "team-budgeting-app-n14",
     Key: {
-      id: event.pathParameters.id,
+      id: event.pathParameters.project_id,
     },
   };
 
-  // delete the todo from the database
+  // delete the project entry from the database
   dynamoDb.delete(params, (error) => {
     // handle potential errors
     if (error) {
@@ -20,7 +20,7 @@ module.exports.delete = (event, context, callback) => {
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t remove the todo item.',
+        body: 'Couldn\'t remove the project item.',
       });
       return;
     }
