8d975267850b5be38c7d29127dc21ff6e87685ae
Kerry <z1454142986@gmail.com>
2021-07-04 21:20:14 -0700

    update README

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
+[![Bugs](https://sonarcloud.io/api/project_badges/measure?project=mjdfx073131_Citi-Budgeting&metric=bugs)](https://sonarcloud.io/dashboard?id=mjdfx073131_Citi-Budgeting) [![Code Smells](https://sonarcloud.io/api/project_badges/measure?project=mjdfx073131_Citi-Budgeting&metric=code_smells)](https://sonarcloud.io/dashboard?id=mjdfx073131_Citi-Budgeting) [![Security Rating](https://sonarcloud.io/api/project_badges/measure?project=mjdfx073131_Citi-Budgeting&metric=security_rating)](https://sonarcloud.io/dashboard?id=mjdfx073131_Citi-Budgeting) [![Duplicated Lines (%)](https://sonarcloud.io/api/project_badges/measure?project=mjdfx073131_Citi-Budgeting&metric=duplicated_lines_density)](https://sonarcloud.io/dashboard?id=mjdfx073131_Citi-Budgeting)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy) 
+
+
+```
+__________                   .___              __   
+\______   \_______  ____   __| _/ ____   _____/  |_ 
+ |     ___/\_  __ \/  _ \ / __ | / ___\_/ __ \   __\
+ |    |     |  | \(  <_> ) /_/ |/ /_/  >  ___/|  |  
+ |____|     |__|   \____/\____ |\___  / \___  >__|  
+                              \/_____/      \/      
+```
 # Citi-Budgeting
-Citi budgeting cloud application
+It is essential for teams within Citi to have a budget planned for each project so that Citi can control their finances and make sure everything runs smoothly
+
+
+# Front-End
+React and Heroku 
+[Repo](https://github.com/LinlinlinlinW/Citi-Budgeting-FE)
+
+# Back-End
+AWS lambda, API Gateway, DynamoDB and Amazon Lex
+
+## Part 1
+FE -> API Gateway -> Lambda -> DynamoDB
+![part1](assets/Screen%20Shot%202021-07-04%20at%2020.48.18.png)
+
+## Part 2
+Initially, trigger lambda directly from `Amazon Lex` 
+Update: trigger lambda using `Axios` to call `API Gateway`
+![part2](assets/Screen%20Shot%202021-07-04%20at%2020.48.29.png)
+
+### Chatbot Demo
+Used `Aws Cognito` to do authentication and deployed Lex to `AWS S3`
+![Chatbot](assets/Screen%20Shot%202021-06-18%20at%2010.07.17.png)
 
 # Google Doc
 [Link](https://docs.google.com/document/d/1UK8MGqmnJMEkaY8PDYpBi0ci2oQbAtCAKsIcuOUUsig/edit?ts=60ca2208)
--- todos/update_request.js
+++ todos/update_request.js
@@ -21,7 +21,7 @@ function dispatch(intentRequest, callback) {
   const slots = intentRequest.currentIntent.slots;
   const project_id = slots.project_id;
   const request_id = slots.request_id;
-  const request_amount = slots.request_amount;
+  const request_amount = parseInt(slots.request_amount,10);
   const request_reason = slots.request_reason;
   const params = {
     TableName: "budget-14-dev",
@@ -61,19 +61,52 @@ function dispatch(intentRequest, callback) {
       statusCode: 200,
       body: JSON.stringify(result.Attributes),
     };
-    callback(null, response);
+      callback(
+        close(sessionAttributes, "Fulfilled", {
+          contentType: "PlainText",
+          content:
+            `Here are the details of your request:\r\n` +
+            "Request ID: " +
+            result.Attributes["request_id"] +
+            "\r\n" +
+            "Project ID: " +
+            result.Attributes["project_id"] +
+            "\r\n" +
+            "Start Date: " +
+            result.Attributes["start_date"] +
+            "\r\n" +
+            "End Date: " +
+            result.Attributes["end_date"] +
+            "\r\n" +
+            "Initial Budget: " +
+            result.Attributes["initial_budget"] +
+            "\n" +
+            "Request Amount: " +
+            result.Attributes["request_amount"] +
+            "\n" +
+            "Net Amount Remaining: " +
+            result.Attributes["net_amount_remaining"] +
+            "\n" +
+            "Team: " +
+            result.Attributes["team"] +
+            "\n" +
+            "Team Manager: " +
+            result.Attributes["team_manager"] +
+            "\n",
+        })
+      );
   });
-  callback(
-    close(sessionAttributes, "Fulfilled", {
-      contentType: "PlainText",
-      content: `Okay, request ${request_id} updated for project ${project_id}. The request amount has been changed to ${request_amount}. Your request_id is ${request_id}.`,
-    })
-  );
+  // callback(
+  //   close(sessionAttributes, "Fulfilled", {
+  //     contentType: "PlainText",
+  //     content: `Okay, request ${request_id} updated for project ${project_id}. The request amount has been changed to ${request_amount}. Your request_id is ${request_id}.`,
+  //   })
+  // );
 }
 // --------------- Main handler -----------------------
 // Route the incoming request based on intent.
 // The JSON body of the request is provided in the event slot.
-module.exports.updateR = async (event, context, callback) => {
+module.exports.updateR = (event, context, callback) => {
   try {
     dispatch(event, (response) => {
         callback(null, response);
